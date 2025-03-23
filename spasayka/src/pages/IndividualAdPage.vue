9b9ce54213c8a9995d27806ae589fa294a9acdda
ritba meow <veselova.rita69@mail.ru>
2025-03-24 02:45:30 +0300

    что то

--- spasayka/src/pages/IndividualAdPage.vue
+++ spasayka/src/pages/IndividualAdPage.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="flex flex-col w-full min-h-screen px-12 py-6">
-    <h2 class="pb-6 font-[Signate_Grotesk] text-2xl">Кого вы ищите?</h2>
-    <h2 class="pb-6 font-[Signate_Grotesk] text-2xl">Кого вы ищите?</h2>
-    <h2 class="pb-6 font-[Signate_Grotesk] text-2xl">Кого вы ищите?</h2>
+    <section class="flex flex-col lg:flex-row gap-10 max-w-[1400px] mx-auto pb-6">
+      <h2 class="pb-6 font-[Signate_Grotesk] text-2xl">Кого вы ищите?</h2>
+      <h2 class="pb-6 font-[Signate_Grotesk] text-2xl">Кого вы ищите?</h2>
+      <h2 class="pb-6 font-[Signate_Grotesk] text-2xl">Кого вы ищите?</h2>
+    </section>
   </div>
 </template>
